e1036cd32f2db1b454949423fd448ac4d7242531
HD M2S <hdm2s@macbook-pro-de-hd.home>
2021-04-13 16:32:26 +0200

    bakery en cours

--- chapitre_04/formulaire/src/App.js
+++ chapitre_04/formulaire/src/App.js
@@ -4,6 +4,27 @@ import './App.css';
 
 class App extends React.Component {
 
+constructor() {
+  super()
+
+  this.state = {
+    validator: ""
+  }
+
+  this.validationEmail = this.validationEmail.bind(this);
+}
+
+validationEmail (e) {
+  if ( e.target.value === /^[^@]+@[^@]+\.[^@]+$/igm ) {
+    this.setState({validator: 'is-valid'})
+    return this.state.validator
+  } else {
+    this.setState({validator: 'is-invalid'})
+    return this.state.validator
+  }
+}
+
+
   render() {
     return (
       <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
@@ -11,11 +32,11 @@ class App extends React.Component {
         <form style={{width: 500}}>
           <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+            <input type="email" className="form-control is-invalid" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
           </div>
           <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1"></input>
+            <input type="password" class="form-control is-invalid" id="exampleInputPassword1"></input>
           </div>
           <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
