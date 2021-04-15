a9717e023c27f4cffd69ddbcdfdab08625faa914
HD M2S <hdm2s@macbook-pro-de-hd.home>
2021-04-15 17:26:32 +0200

    katas

--- katas/united-state-of-react/src/components/Button-usa.jsx
+++ katas/united-state-of-react/src/components/Button-usa.jsx
@@ -5,7 +5,9 @@ class Button extends React.Component {
     render() {
         return (
             <div>
+
                 <button type="button" className={this.props.className} onClick={this.props.onClick}>{this.props.children}</button>
+                
             </div>
         )
     }
