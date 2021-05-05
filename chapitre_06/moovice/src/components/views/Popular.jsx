75b0527be6314f06b73cc80b617a05a59e4dcd5c
Moncef <moncefboughal@hotmail.fr>
2021-05-05 17:26:51 +0200

    in the bonus

--- chapitre_06/moovice/src/components/views/Popular.jsx
+++ chapitre_06/moovice/src/components/views/Popular.jsx
@@ -8,7 +8,8 @@ class Popular extends React.Component {
         super()
 
         this.state = {
-            movies: []
+            movies: [],
+            page: 1
         }
     }
 
@@ -20,9 +21,33 @@ class Popular extends React.Component {
                     movies: response.results
                 })
             })
+
+        window.addEventListener('scroll', function (e) {
+            if ((e.target.scrollingElement.scrollHeight - e.target.scrollingElement.scrollTop) === e.target.scrollingElement.clientHeight) {
+                component.forceUpdate(callback)
+            }
+        })
+    }
+
+    componentDidUpdate() {
+        let currentPage = this.state.page || 1
+        let newPage = currentPage + 1
+        fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bf01af8f5d704591a09edcc0c2f5e084" + "&page=" + newPage)
+            .then(res => res.json())
+            .then(response => {
+
+                const allMovies = this.state.movies
+                allMovies.push(response.results)
+
+                this.setState({
+                    movies: allMovies,
+                    page: this.state.page + 1
+                })
+            })
     }
 
     render() {
+
 
         return (
             <div>
@@ -31,9 +56,9 @@ class Popular extends React.Component {
                 <div>
                     {this.state.movies.map(elem => {
                         return <Card image={"https://image.tmdb.org/t/p/w500" + elem.poster_path}
-                                     title={elem.title}
-                                      year={elem.release_date}
-                                      description={elem.overview}  />
+                            title={elem.title}
+                            year={elem.release_date}
+                            description={elem.overview} />
                     })}
                 </div>
             </div>
